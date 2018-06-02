1425db17e116555f67661fde69a3e9411db1e716
Omar Abdelkader <omikader@gmail.com>
2018-06-02 17:23:10 -0400

    yapf formatting

--- src/gan.py
+++ src/gan.py
@@ -10,66 +10,100 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data', default='../data', metavar='DIR',
-    help='path to dataset')
-parser.add_argument('--batch-size', type=int, default=50, metavar='N',
+parser.add_argument(
+    '--data', default='../data', metavar='DIR', help='path to dataset')
+parser.add_argument(
+    '--batch-size',
+    type=int,
+    default=50,
+    metavar='N',
     help='input batch size for training (default: 50)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument(
+    '--epochs',
+    type=int,
+    default=10,
+    metavar='N',
     help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+parser.add_argument(
+    '--lr',
+    type=float,
+    default=0.01,
+    metavar='LR',
     help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+parser.add_argument(
+    '--momentum',
+    type=float,
+    default=0.5,
+    metavar='M',
     help='SGD momentum (default: 0.5)')
 args = parser.parse_args()
+
 
 def pil_loader(path):
     with open(path, 'rb') as f:
         with Image.open(f) as img:
             return img.convert('RGBA')
 
+
 def get_flag_loader(train_dir, batch_size, shuffle=True):
     transform = transforms.ToTensor()
     flag_dataset = datasets.ImageFolder(
         root=train_dir, transform=transform, loader=pil_loader)
     flag_loader = torch.utils.data.DataLoader(
-        flag_dataset, batch_size=batch_size, shuffle=shuffle)
+        dataset=flag_dataset, batch_size=batch_size, shuffle=shuffle)
 
     return flag_loader
 
-def data_generator(batch_size):
-    return torch.FloatTensor(batch_size, 4, 16, 32).uniform_(0, 255)
 
-def train_d(data_loader, data_generator, discriminator, generator, optimizer, loss):
+def train_d(data_loader, discriminator, generator, optimizer, loss_fcn):
     for data, _ in data_loader:
-        discriminator.zero_grad() # Zero out gradients on discriminator
+        # Zero out gradients on discriminator
+        discriminator.zero_grad()
 
-        real_data = Variable(data) # Load real flag data
-        output = discriminator(real_data) # Run flag data through the discriminator
-        real_target = Variable(torch.ones(args.batch_size, 1)) # Compare against all ones, because data is legit
-        real_error = loss(output, real_target) # Compute BCE loss based on legit data
-        real_error.backward() # Compute new gradients, but don't update yet
+        # Load real flag data, run through discriminator and compute BCE loss
+        # against target vector of all ones, because the flags are legit.
+        real_data = Variable(data)
+        output = discriminator(real_data)
+        real_target = Variable(torch.ones(args.batch_size, 1))
+        real_error = loss(output, real_target)
 
-        gen_input = Variable(data_generator(args.batch_size)) # Get uniformly distributed input data
-        fake_data = generator(gen_input) # Map input data to create fake flag data using generator
-        decision = discriminator(fake_data) # Run the mapped data through the discriminator
-        fake_target = Variable(torch.zeros(args.batch_size, 1)) # Compare against all zeros, because data is fake
-        fake_error = loss(decision, fake_target) # Compute BCE loss based on fake data
-        fake_error.backward() # Compute new gradients
+        # Get uniformly distributed noise and feed to generator to create fake
+        # flag data. Run fake flag data through discriminator and compute BCE
+        # loss against target vector of all zeros, because data is fake.
+        gen_input = Variable(
+            torch.FloatTensor(args.batch_size, 100).uniform_(0, 255))
+        fake_data = generator(gen_input)
+        decision = discriminator(fake_data)
+        fake_target = Variable(torch.zeros(args.batch_size, 1))
+        fake_error = loss(decision, fake_target)
 
-        optimizer.step() # Update discriminator weights based on both sets of gradients
+        # Compute accumulated gradient based on real and fake data to update
+        # discriminator weights
+        (real_error + fake_error).backward()
+        optimizer.step()
 
-def train_g(data_generator, discriminator, generator, optimizer, n_batches, loss):
+
+def train_g(n_batches, discriminator, generator, optimizer, loss_fcn):
     for i in range(n_batches):
-        generator.zero_grad() # Zero out gradients on generator
+        # Zero out gradients on generator
+        generator.zero_grad()
 
-        gen_input = Variable(data_generator(args.batch_size)) # Get uniformly distributed input data
-        fake_data = generator(gen_input) # Map input data to create fake flag data using generator
-        decision = discriminator(fake_data) # Run the mapped data through the discriminator
-        target = Variable(torch.ones(args.batch_size, 1)) # Want to fool discriminator so pretend mapped data is genuine
-        g_error = loss(decision, target) # Compute BCE loss on generator
-        g_error.backward() # Compute new gradients
+        # Get uniformly distributed noise and feed to generator to create fake
+        # flag data. Run fake flag data through discriminator and compute BCE
+        # loss against target vector of all ones. We want to fool the
+        # discriminator, so preten the mapped data is genuine
+        gen_input = Variable(
+            torch.FloatTensor(args.batch_size, 100).uniform_(0, 255))
+        fake_data = generator(gen_input)
+        decision = discriminator(fake_data)
+        target = Variable(torch.ones(args.batch_size, 1))
+        g_error = loss(decision, target)
 
-        optimizer.step() # Update generator weights based new gradients
+        # Compute new gradients from discriminator and update weights of the
+        # generator
+        g_error.backward()
+        optimizer.step()
+
 
 if __name__ == '__main__':
     flag_loader = get_flag_loader(args.data, args.batch_size)
@@ -81,5 +115,15 @@ if __name__ == '__main__':
     g_optimizer = optim.SGD(G.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
-        train_d(flag_loader, data_generator, D, G, d_optimizer, loss)
-        train_g(data_generator, D, G, g_optimizer, NUM_BATCHES, loss)
+        train_d(
+            data_loader=flag_loader,
+            discriminator=D,
+            generator=G,
+            optimizer=d_optimizer,
+            loss_fcn=loss)
+        train_g(
+            n_batches=NUM_BATCHES,
+            discriminator=D,
+            generator=G,
+            optimizer=g_optimizer,
+            loss_fcn=loss)
--- src/models.py
+++ src/models.py
@@ -1,24 +1,28 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Generator(nn.Module):
+
     def __init__(self):
         super(Generator, self).__init__()
-        self.fc1 = nn.Linear(2048, 512)
-        self.fc2 = nn.Linear(512, 512)
+        self.fc1 = nn.Linear(100, 1024)
+        self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 2048)
 
     def forward(self, x):
-        x = x.view(-1, 2048)
+        x = x.view(-1, 100)
         x = F.relu(self.fc1(x))
         x = F.sigmoid(self.fc2(x))
         return self.fc3(x)
 
+
 class Discriminator(nn.Module):
+
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.fc1 = nn.Linear(2048, 512)
-        self.fc2 = nn.Linear(512, 512)
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 1)
 
     def forward(self, x):
