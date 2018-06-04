bb3e9bf6099408cca751d7228bb83980ec3ad446
Omar Abdelkader <omikader@gmail.com>
2018-06-04 03:01:29 -0400

    Implement DCGAN

--- src/models.py
+++ src/models.py
@@ -6,26 +6,43 @@ class Generator(nn.Module):
 
     def __init__(self):
         super(Generator, self).__init__()
-        self.fc1 = nn.Linear(100, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 2048)
+        self.fc1 = nn.Linear(100, 2048)
+        self.fc1_bn = nn.BatchNorm2d(2048)
+        self.conv1 = nn.ConvTranspose2d(1024, 512, 4, 2, 1)
+        self.conv1_bn = nn.BatchNorm2d(512)
+        self.conv2 = nn.ConvTranspose2d(512, 256, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(256)
+        self.conv3 = nn.ConvTranspose2d(256, 128, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.conv4 = nn.ConvTranspose2d(128, 4, 4, 2, 1)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.sigmoid(self.fc2(x))
-        return self.fc3(x)
+        x = F.relu(self.fc1_bn(self.fc1(x)))
+        x = x.view(-1, 1024, 1, 2)  # reshape
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
+        x = F.relu(self.conv3_bn(self.conv3(x)))
+        x = F.tanh(self.conv4(x))
+        return x
 
 
 class Discriminator(nn.Module):
 
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.fc1 = nn.Linear(2048, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 1)
+        self.conv1 = nn.Conv2d(4, 128, 4, 2, 1)
+        self.conv2 = nn.Conv2d(128, 256, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(256, 512, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(512)
+        self.conv4 = nn.Conv2d(512, 1024, 4, 2, 1)
+        self.conv4_bn = nn.BatchNorm2d(1024)
+        self.conv5 = nn.Conv2d(1024, 1, (1, 2), 1, 0)
 
     def forward(self, x):
-        x = x.view(-1, 2048)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return F.sigmoid(self.fc3(x))
+        x = F.leaky_relu(self.conv1(x))
+        x = F.leaky_relu(self.conv2_bn(self.conv2(x)))
+        x = F.leaky_relu(self.conv3_bn(self.conv3(x)))
+        x = F.leaky_relu(self.conv4_bn(self.conv4(x)))
+        x = F.sigmoid(self.conv5(x))
+        return x
